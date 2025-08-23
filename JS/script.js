0a8e839966fff355f7781381919be317c366666a
Najla Azizah Fairuz <najlaafairuz963@gmail.com>
2025-08-23 09:05:18 +0700

    Add todo list functionality with task management and update HTML structure

--- JS/script.js
+++ JS/script.js
@@ -3,35 +3,39 @@ const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
 const todoDate = document.querySelector('.todo-date');
 const todoList = document.querySelector('.todo-list');
-const deleteAllbtn = document.querySelector('.delete-all-button');
-
+const deleteAllbutton = document.querySelector('.deleteall-button');
+const noTaskMessage = document.querySelector('.no-task-message');
 
 //eventlistener untuk menjalankan fungsi addtodo taip kali form di submit
 todoForm.addEventListener('submit',addTodo);
 todoList.addEventListener('click', handleTaskActions)
-deleteAllbtn.addEventListener('click',deleteAllTasks);
-
+deleteAllbutton.addEventListener('click',deleteAllTasks);
+document.addEventListener('DOMContentLoaded', updateUI);
 //function, bagian yang menjlankan 
 function addTodo(event) {
+    event.preventDefault();
+
     //Mencegah form dari aksi default-nya (refresh halamann event.preventDefault();
     //mengambil nilai dari inputan user
     const todoText = todoInput.value;
     const dateValue = todoDate.value;
-}
-//membuat html to do task
-const todoTaskHTML = `
+    const todoTaskHTML = `
     <li class="todo-task">
         <span class="todo-text">${todoText}</span>
         <span class="due-date">${dateValue}</span>
-        <div class="-actions">
+        <div class="task-actions"> 
             <button class="complete-button">Selesai</button>
+            <button class="delete-button">Hapus</button> 
         </div>
     </li>
-
 `;
 todoList.insertAdjacentHTML('beforeend', todoTaskHTML);
 todoInput.value ='';
 todoDate.value='';
+updateUI()
+}
+//membuat html to do task
+
 
 function handleTaskActions(e){
 const clickedElement = e.target;
@@ -41,13 +45,17 @@ if (clickedElement.classList.contains('delete-button')){
 } else if (clickedElement.classList.contains('complete-button')){
     const todoTask = clickedElement.closest('.todo-task');
     todoTask.classList.toggle('completed');
-    const completeButton = todoTask.querySelector('.complete-button');
-    completeButton.textContent = todoTask.classList.contains('completed') ? 'Belum Selesai' : 'Selesai';
 }
 }
 function deleteAllTasks() {
     todoList.innerHTML = '';
+    updateUI();
 }
+function updateUI() {
 
-
-
+    if (todoList.children.length > 0) {
+        noTaskMessage.style.display = 'none';
+    } else {
+        noTaskMessage.style.display = 'block';
+    }
+}
